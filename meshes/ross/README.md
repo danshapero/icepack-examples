ed377f8e57f9be1641fc258dc22f6df01d29539d
danshapero <shapero.daniel@gmail.com>
2016-01-24 17:39:35 -0800

    Extracting Ross boundary PSLG and writing to .poly

--- meshes/ross/README.md
+++ meshes/ross/README.md
@@ -5,3 +5,8 @@ The mesh is originally hosted at:
 [http://websrv.cs.umt.edu/isis/index.php/Ice_shelves](http://websrv.cs.umt.edu/isis/index.php/Ice_shelves)
 
 Near as I can tell, the mesh was made in COMSOL by Todd Dupont and Olga Sergienko.
+
+The Python script `fetch` retrieves the mesh from the interwebs and unpacks it from the original format, a Matlab `.mat` archive.
+The `munge` script does various manipulations to turn the mesh into a format I can use.
+In particular, I'd like to take the original domain and remesh it at a coarser resolution using a quad mesh, so that I can do computations with [deal.II](http://www.dealii.org).
+This involves extracting the boundary from the original mesh.
--- meshes/ross/munge.py
+++ meshes/ross/munge.py
@@ -1,9 +1,155 @@
 
 import scipy.io
 import numpy as np
+from scipy.sparse import lil_matrix
+from matplotlib.path import Path
 
 import formats.triangle
 import formats.gmsh
+
+
+# ===========================================================================
+# The following routines are used to turn the original triangular mesh of the
+# Ross Ice Shelf into a planar straight-line graph (PSLG) of its boundary.
+#
+# This is a somewhat complicated operation. We only have the node coordinates,
+# indices of the nodes in each triangle, and boundary markers of the original
+# triangulation. To get extract the PSLG of the boundary, we need to extract
+# the boundary from the triangulation with a consistent numbering. Then we
+# need to find the connected components of the boundary; for example, there is
+# one component corresponding to the whole inflow and front of RIS, and one
+# component corresponding to Roosevelt Island. Then we need to determine which
+# components are in the interior so that we can find points inside them; these
+# hole points are necessary for writing a PSLG in Triangle's format.
+# ===========================================================================
+
+
+# ------------------------------------
+def get_boundary_graph(triangles, bnd):
+    """
+    Return a sparse graph representing the boundary of a triangulation
+    """
+
+    num_nodes = len(bnd)
+    num_bnd_points = sum(bnd != 0)
+    index = np.zeros(num_bnd_points, dtype = np.int32)
+    index_inverse = np.zeros(num_nodes, dtype = np.int32)
+    count = 0
+    for i in range(num_nodes):
+        if bnd[i] != 0:
+            index[count] = i
+            count += 1
+
+    for i in range(num_bnd_points):
+        index_inverse[index[i]] = i
+
+    g = lil_matrix((num_bnd_points, num_bnd_points), dtype = np.int32)
+    num_triangles, _ = np.shape(triangles)
+    for n in range(num_triangles):
+        triangle = triangles[n, :]
+        for k in range(3):
+            i, j = triangle[k], triangle[(k + 1) % 3]
+            if bnd[i] != 0 and bnd[j] != 0:
+                weight = max(bnd[i], bnd[j])
+                I, J = index_inverse[[i, j]]
+                g[I, J] = weight
+                g[J, I] = weight
+
+    return g, index
+
+
+# -------------------------
+def connected_components(g):
+    """
+    Given a graph, stored as a scipy sparse matrix, return a list of its
+    components
+    """
+    num_nodes, _ = np.shape(g)
+
+    components = []
+    visited = np.zeros(num_nodes, dtype = bool)
+
+    # TODO: make this not so stupid and inefficient
+    while not all(visited):
+        # TODO: make this also not so stupid and inefficient
+        i = np.argmin(visited)
+        visited[i] = True
+        stack = [i]
+        current_component = []
+
+        while stack:
+            i = stack.pop()
+            current_component.append(i)
+            _, neighbors = g[i, :].nonzero()
+
+            for j in neighbors:
+                if not visited[j]:
+                    visited[j] = True
+                    stack.append(j)
+
+        components.append(current_component)
+
+    return components
+
+
+# ------------------------
+class GoofyPSLG(Exception):
+    pass
+
+
+# -----------------------------------------
+def outermost_path(components, index, x, y):
+    """
+    Given a set of connected components of the boundary, find which component
+    contains all the others
+    """
+
+    # Make a list of lists of the Path object corresponding to each component
+    # of the boundary
+    gammas = [Path(list(zip(x[index[component]],
+                            y[index[component]])),
+                   closed = True)
+              for component in components]
+
+    # Find which component contains all others
+    num_components = len(gammas)
+    for i in range(num_components):
+        gamma_i = gammas[i]
+        for j in range(i + 1, num_components):
+            gamma_j = gammas[j]
+            if gamma_i.contains_path(gamma_j):
+                return i
+
+    # I have no idea how this could happen but I'd sure like to know if it does
+    raise GoofyPSLG()
+
+
+# ---------------------------------------------------------
+def get_holes(components, outermost_component, index, x, y):
+    """
+    To generate a PSLG that Triangle can read, we need to find a point within
+    each hole of the triangulation
+    """
+    xh, yh = [], []
+    for n in range(len(components)):
+        if n != outermost_component:
+            component = components[n]
+            gamma = Path(list(zip(x[index[component]],
+                                  y[index[component]])),
+                         closed = True)
+
+            X, Y = 0.0, 0.0
+            i, j = 0, len(gamma)/2 - 1
+
+            while not gamma.contains_point((X, Y)):
+                j += 1
+                X, Y = 0.5 * (gamma.vertices[i,:] + gamma.vertices[j,:])
+
+            xh.append(X)
+            yh.append(Y)
+
+    return np.array(xh), np.array(yh)
+
 
 if __name__ == "__main__":
     ross = scipy.io.loadmat("Mesh_RIS.mat")
@@ -20,5 +166,21 @@ if __name__ == "__main__":
 
     triangles = np.asarray([[k-1 for k in t] for t in ross['index']])
 
-    formats.triangle.write("ross.1", x, y, bnd, triangles)
-    formats.gmsh.write("ross.msh", x, y, bnd, triangles)
+    formats.triangle.write("ross_original.1", x, y, bnd, triangles)
+    formats.gmsh.write("ross_original.msh", x, y, bnd, triangles)
+
+    g, index = get_boundary_graph(triangles, bnd)
+    components = connected_components(g)
+    outermost = outermost_path(components, index, x, y)
+    xh, yh = get_holes(components, outermost, index, x, y)
+
+    bnd_edges = []
+    for component in components:
+        n = len(component)
+        for i in range(n):
+            bnd_edges.append([component[i], component[(i + 1) % n]])
+    bnd_edges = np.asarray(bnd_edges)
+
+    formats.triangle.write_poly("ross.poly",
+                                x[index], y[index], bnd[index],
+                                bnd_edges, xh, yh)
