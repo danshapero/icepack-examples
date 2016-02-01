a0aa9ce88b22b0c605578bdab22a37e876d8c6ad
danshapero <shapero.daniel@gmail.com>
2016-02-01 10:17:44 -0800

    Wrote munging script for bedmap thicknesses

--- thickness/antarctica/munge.py
+++ thickness/antarctica/munge.py
@@ -7,7 +7,7 @@ import regions
 from gis import arcinfo
 
 if __name__ == "__main__":
-    dem = gdal.Open("krigged_dem_nsidc.bin")
+    dem = gdal.Open("bedmap2_bin/bedmap2_thickness.flt")
 
     gt = dem.GetGeoTransform()
     Xmin, Ymax = gt[0], gt[3]
@@ -15,7 +15,9 @@ if __name__ == "__main__":
 
     nx, ny = dem.RasterXSize, dem.RasterYSize
     raster_band = dem.GetRasterBand(1)
-    s = raster_band.ReadAsArray()
+    no_data = raster_band.GetNoDataValue()
+
+    h = raster_band.ReadAsArray()
 
     for name, region in regions.antarctica.items():
         xmin, xmax = region['x']
@@ -27,9 +29,9 @@ if __name__ == "__main__":
         jmax = int(math.ceil((xmax - Xmin) / dx))
         jmin = int(math.floor((xmin - Xmin) / dx))
 
-        s_region = (s[imax:imin, jmin:jmax])[::-1,:]
+        h_region = (h[imax:imin, jmin:jmax])[::-1,:]
 
         x = np.linspace(Xmin + jmin*dx, Xmin + jmax*dx, jmax - jmin, False)
         y = np.linspace(Ymax - imin*dx, Ymax - imax*dx, imin - imax, False)
 
-        arcinfo.write(name.lower() + "-s.txt", x, y, s_region, -999.0)
+        arcinfo.write(name.lower() + "-h.txt", x, y, h_region, no_data)
