4c1b82b34ea3a31ce0c26d38f8cc3a754ea7a40c
danshapero <shapero.daniel@gmail.com>
2016-02-05 14:36:26 -0800

    Antarctica thickness munging outputs ice mask

--- thickness/antarctica/munge.py
+++ thickness/antarctica/munge.py
@@ -8,16 +8,17 @@ from gis import arcinfo
 
 if __name__ == "__main__":
     dem = gdal.Open("bedmap2_bin/bedmap2_thickness.flt")
+    icemask = gdal.Open("bedmap2_bin/bedmap2_icemask_grounded_and_shelves.flt")
 
     gt = dem.GetGeoTransform()
     Xmin, Ymax = gt[0], gt[3]
     dx, dy = gt[1], gt[5]
 
-    nx, ny = dem.RasterXSize, dem.RasterYSize
     raster_band = dem.GetRasterBand(1)
     no_data = raster_band.GetNoDataValue()
-
     h = raster_band.ReadAsArray()
+
+    mask = icemask.GetRasterBand(1).ReadAsArray()
 
     for name, region in regions.antarctica.items():
         xmin, xmax = region['x']
@@ -30,8 +31,10 @@ if __name__ == "__main__":
         jmin = int(math.floor((xmin - Xmin) / dx))
 
         h_region = (h[imax:imin, jmin:jmax])[::-1,:]
+        mask_region = (mask[imax:imin, jmin:jmax])[::-1,:]
 
         x = np.linspace(Xmin + jmin*dx, Xmin + jmax*dx, jmax - jmin, False)
         y = np.linspace(Ymax - imin*dx, Ymax - imax*dx, imin - imax, False)
 
         arcinfo.write(name.lower() + "-h.txt", x, y, h_region, no_data)
+        arcinfo.write(name.lower() + "-mask.txt", x, y, mask_region, no_data)
