95e66b1866dc959c169c6b72005961ea727ec067
danshapero <shapero.daniel@gmail.com>
2016-08-03 12:50:24 -0700

    Added routine for reading gmsh quad mesh

--- meshes/gmsh.py
+++ meshes/gmsh.py
@@ -1,8 +1,11 @@
 
 import numpy as np
 
-# ---------------------------------------
-def write(filename, x, y, bnd, triangles):
+# -------------------------------------------
+def write_tri(filename, x, y, bnd, triangles):
+    """
+    Write a triangular mesh to the gmsh .msh format.
+    """
     num_nodes = len(x)
     num_triangles, _ = np.shape(triangles)
 
@@ -23,3 +26,41 @@ def write(filename, x, y, bnd, triangles):
             t = [k + 1 for k in triangles[n, :]]
             msh_file.write("{0} 2 0 {1} {2} {3}\n".format(n+1, t[0], t[1], t[2]))
         msh_file.write("$Endelements\n")
+
+    return
+
+
+# ---------------------
+def read_quad(filename):
+    """
+    Read an unstructured quad mesh in the gmsh .msh format.
+    """
+    with open(filename, "r") as msh_file:
+        for k in range(4):
+            msh_file.readline()
+
+        num_nodes = int(msh_file.readline())
+        x, y = np.zeros(num_nodes), np.zeros(num_nodes)
+
+        for k in range(num_nodes):
+            line = msh_file.readline().split()
+            x[k], y[k] = float(line[1]), float(line[2])
+
+        for k in range(2):
+            msh_file.readline()
+
+        num_elements = int(msh_file.readline())
+        quads = []
+        for k in range(num_elements):
+            line = [int(s) for s in msh_file.readline().split()]
+            element_type = line[1]
+
+            if element_type == 3:
+                num_tags = line[2]
+                start_index = 3 + num_tags
+                quad = [n - 1 for n in line[start_index: start_index + 4]]
+                quads.append(quad)
+
+        quads = np.array(quads)
+
+    return x, y, quads
