8c9125580f4b753a5af3655b6cf11e505ecc2f17
danshapero <shapero.daniel@gmail.com>
2016-09-16 10:38:00 -0700

    Moved prefatory stuff in example 1 to separate file.

--- examples/01-square-ice-shelf/square_ice_shelf.cpp
+++ examples/01-square-ice-shelf/square_ice_shelf.cpp
@@ -1,51 +1,11 @@
+// In this example, we'll see how to use the basic functionality of icepack --
+// creating a mesh, creating some scalar and vector fields, and solving the
+// diagnostic equations for the velocity of an ice shelf.
 
-#include <deal.II/base/function.h>
-#include <deal.II/grid/grid_generator.h>
-
-#include <icepack/physics/constants.hpp>
-#include <icepack/physics/viscosity.hpp>
-#include <icepack/glacier_models/ice_shelf.hpp>
-
-using dealii::Tensor;
-using dealii::Point;
-
-using icepack::Field;
-using icepack::VectorField;
-using icepack::rho_ice;
-using icepack::rho_water;
-using icepack::gravity;
-
-// Some physical constants
-const double rho = rho_ice * (1 - rho_ice / rho_water);
-const double temp = 263.15;
-const double delta_temp = -10.0;
-const double A = pow(rho * gravity / 4, 3) * icepack::rate_factor(temp);
-
-const double u0 = 100.0;
-const double length = 20000.0, width = 20000.0;
-const double h0 = 600.0;
-const double delta_h = 300.0;
-
-// Type alias
-template <int dim>
-using Fn = dealii::ScalarFunctionFromFunctionObject<dim>;
-
-
-class Velocity : public dealii::TensorFunction<1, 2>
-{
-public:
-  Velocity() {}
-  Tensor<1, 2> value(const Point<2>& x) const
-  {
-    const double q = 1 - pow(1 - delta_h * x[0] / (length * h0), 4);
-
-    Tensor<1, 2> v; v[1] = 0.0;
-    v[0] = u0 + 0.25 * A * q * length * pow(h0, 4) / delta_h;
-
-    return v;
-  }
-};
-
+// This file contains a bunch of import statements, definitions of physical
+// constants, etc. -- it's not too relevant, so we've put it in a separate
+// file.
+#include "data.hpp"
 
 int main()
 {
@@ -65,7 +25,8 @@ int main()
       if (cell->face(face_number)->center()(0) > length - 1.0)
         cell->face(face_number)->set_boundary_id(1);
 
-  // Now refine the mesh a few times so that the solution is accurate enough.
+  // Now refine the mesh a few times so that our solution will be decently
+  // accurate.
   const unsigned int num_levels = 5;
   mesh.refine_global(num_levels);
 
@@ -74,9 +35,13 @@ int main()
   // linear ramp which thins out towards the ice front.
   const Fn<2> thickness([&](const Point<2>& x)
                         {return h0 - delta_h/length * x[0];});
+
+  // The velocity is defined in the data file -- it's the exact solution of the
+  // diagnostic equations with the thickness above and a constant temperature
+  // of -10C.
   const Velocity velocity;
 
-  // To keep things interesting, we'll perturb the ice velocity a bit in the
+  // But to keep things interesting, we'll perturb the temperature a bit in the
   // center of the domain.
   const Fn<2> temperature([&](const Point<2>& x)
                           {
