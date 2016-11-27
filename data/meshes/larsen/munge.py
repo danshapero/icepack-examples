bccd7720b2f63c144c503d95e4215b301f6b95e2
danshapero <shapero.daniel@gmail.com>
2016-11-26 18:42:07 -0800

    Added script + geojson files for rough Larsen mesh

---
+++ data/meshes/larsen/munge.py
@@ -0,0 +1,34 @@
+
+import geojson
+from icepack.mesh import stitch, geo
+
+
+# ----------------------------
+class GeometryError(Exception):
+    def __init__(self, geometry_type):
+        self.geometry_type = geometry_type
+
+    def __str__(self):
+        return geometry_type
+
+
+# -----------------------
+if __name__ == "__main__":
+    input_filenames = ["larsen_front0.geojson",
+                       "larsen_inflow.geojson",
+                       "larsen_side0.geojson",
+                       "larsen_side1.geojson"]
+
+    Xs = []
+    for input_filename in input_filenames:
+        with open(input_filename, "r") as input_file:
+            dataset = geojson.loads(input_file.read())
+            for feature in dataset['features']:
+                if feature['geometry']['type'] != 'LineString':
+                    raise GeometryError(feature['geometry']['type'])
+
+                X = feature['geometry']['coordinates']
+                Xs.append(X)
+
+    Xs, successors = stitch.segment_successors(Xs, tol = 2.0e4)
+    geo.write("larsen.geo", Xs, successors, dx = 5.0e4, quad = True)
